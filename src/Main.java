259b344d317b5fa7811938e5927f1e859c2de7ed
konorevph <regouserk@mail.ru>
2023-09-25 03:28:25 +0300

    Creating based on list algorithm

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import ProcessorSimulator.OperatingSystem;
+import ProcessorSimulator.Process;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        OperatingSystem operatingSystem = new OperatingSystem(3, 2);
+        operatingSystem.createProcess("2 (2) 5 (0) 3");
+        operatingSystem.createProcess("2 (2) 6 (2) 2");
+        operatingSystem.createProcess("2 (1) 4 (1) 1");
+        operatingSystem.createProcess("3 (2) 3 (0) 2");
+        operatingSystem.ProcessesCalculation();
     }
 }
